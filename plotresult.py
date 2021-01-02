aa00e6ea018ddc06305de7d804a7255df7a6a279
Elliott Mahler <join.together@gmail.com>
2021-01-02 12:54:07 -0800

    Mixed radix AVX comparisons

--- plotresult.py
+++ plotresult.py
@@ -1,105 +1,37 @@
 import csv
 from matplotlib import pyplot as plt
 import matplotlib.ticker as ticker
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 import sys
 import math
+import codecs
 
 fname = sys.argv[1]
 data = []
-with open(fname) as f:
-    reader = csv.reader(f)
-    headers = next(reader)
-    for header in headers:
-        data.append([])
-    for line in reader:
-        for n, val in enumerate(line):
-            data[n].append(float(val))
+result_file = codecs.open(fname, 'rU', 'utf-16')
+reader = csv.reader(result_file)
+headers = next(reader)
+for header in headers:
+    data.append([])
+for line in reader:
+    for n, val in enumerate(line):
+        data[n].append(float(val))
 
 #print(data)
 x = data[0]
 ys = data[1:]
 fftnames = headers[1:]
 
-ys_norm = []
-for y in ys:
-    ynorm = [1000*y/(n*math.log(n)) for n, y in zip(x,y)]
-    ys_norm.append(ynorm)
-
-y_combo = [min((a,b,c)) for a, b, c in zip(ys[0], ys[2], ys[3])]
-
-y_div12 = [y1/y2 for y1, y2 in zip(ys[0], ys[1])]
-
-y_div32 = [y3/y2 for y3, y2 in zip(ys[2], ys[1])]
-
-y_div42 = [y4/y2 for y4, y2 in zip(ys[3], ys[1])]
-
-y_div31 = [y3/y1 for y3, y1 in zip(ys[2], ys[0])]
-
-y_div41 = [y4/y1 for y4, y1 in zip(ys[3], ys[0])]
-
-y_divcombo = [yc/yfftw for yc, yfftw in zip(y_combo, ys[1])]
-
 fig1 = plt.figure(1, figsize=(10,7))
 for y in ys: 
     plt.plot(x,y, linestyle='dashed', marker=".")
 plt.legend(fftnames)
-plt.xlabel("Length")
-plt.ylabel("Time, us")
-
-fig2 = plt.figure(2, figsize=(10,7))
-for y in ys_norm: 
-    plt.plot(x,y)
-plt.legend(fftnames)
-plt.xlabel("Length")
-plt.ylabel("Time/NlogN")
-
-fig3 = plt.figure(3, figsize=(10,7)) 
-plt.plot(x,y_div12)
-plt.grid(True)
-fig3.gca().yaxis.set_major_locator(ticker.MultipleLocator(1))
-fig3.gca().xaxis.set_major_locator(ticker.MultipleLocator(32))
-plt.xlabel("Length")
-plt.ylabel("{}/{}".format(fftnames[0], fftnames[1]))
-
-fig4 = plt.figure(4, figsize=(10,7)) 
-plt.plot(x,y_div32)
-plt.grid(True)
-fig4.gca().yaxis.set_major_locator(ticker.MultipleLocator(1))
-fig4.gca().xaxis.set_major_locator(ticker.MultipleLocator(32))
-plt.xlabel("Length")
-plt.ylabel("{}/{}".format(fftnames[2], fftnames[1]))
-
-fig5 = plt.figure(5, figsize=(10,7)) 
-plt.plot(x,y_div42)
-plt.grid(True)
-fig5.gca().yaxis.set_major_locator(ticker.MultipleLocator(1))
-fig5.gca().xaxis.set_major_locator(ticker.MultipleLocator(32))
-plt.xlabel("Length")
-plt.ylabel("{}/{}".format(fftnames[3], fftnames[1]))
-
-fig6 = plt.figure(6, figsize=(10,7)) 
-plt.plot(x,y_div31)
-plt.grid(True)
-fig6.gca().yaxis.set_major_locator(ticker.MultipleLocator(1))
-fig6.gca().xaxis.set_major_locator(ticker.MultipleLocator(32))
-plt.xlabel("Length")
-plt.ylabel("{}/{}".format(fftnames[2], fftnames[0]))
-
-fig7 = plt.figure(7, figsize=(10,7)) 
-plt.plot(x,y_div41)
-plt.grid(True)
-fig7.gca().yaxis.set_major_locator(ticker.MultipleLocator(1))
-fig7.gca().xaxis.set_major_locator(ticker.MultipleLocator(32))
-plt.xlabel("Length")
-plt.ylabel("{}/{}".format(fftnames[3], fftnames[0]))
-
-fig8 = plt.figure(8, figsize=(10,7)) 
-plt.plot(x,y_divcombo)
-plt.grid(True)
-fig8.gca().yaxis.set_major_locator(ticker.MultipleLocator(1))
-fig8.gca().xaxis.set_major_locator(ticker.MultipleLocator(32))
-plt.xlabel("Length")
-plt.ylabel("Combo/FFTW")
+plt.xlabel("Inner FFT Size")
+plt.ylabel("Multiplier")
+plt.grid(True, 'both')
+axes = fig1.gca()
+axes.xaxis.set_major_locator(MultipleLocator(32768))
+axes.yaxis.set_major_locator(MultipleLocator(1))
+axes.yaxis.set_minor_locator(MultipleLocator(0.25))
 
 plt.show()
-#print(data)
--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,58 @@
-use rustfft::FFTplanner;
-use rustfft::num_complex::Complex;
-use rustfft::num_traits::Zero;
-use std::time::Instant;
-use fftw::array::AlignedVec;
-use fftw::plan::*;
-use fftw::types::*;
-use chfft::CFft1D;
-use num_complex;
-use num_traits;
+use std::{sync::Arc, time::Instant};
 
-use fourier::create_fft_f64;
-use fourier::Fft;
-use fourier::Transform;
+use num_complex::Complex;
+use num_traits::Zero;
+use rustfft::{Fft, FftPlannerAvx, avx::*};
 
-fn measure_rustfft(n:usize) -> f32 {
-    let mut input:  Vec<Complex<f64>> = vec![Complex::zero(); n];
-    input[0] = Complex::from(1.0);
-    let mut output: Vec<Complex<f64>> = vec![Complex::zero(); n];
-    let mut planner = FFTplanner::new(false);
-    let fft = planner.plan_fft(n);
-    let reps = 10000/n + 1;
-    for _r in 0..reps/4 {
-        fft.process(&mut input, &mut output);
+fn measure_fft(fft: Arc<dyn Fft<f32>>) -> f32 {
+    let mut buffer = vec![Complex::zero(); fft.len()];
+    let mut scratch = vec![Complex::zero(); fft.get_inplace_scratch_len()];
+    let reps = 1_000_000_000/fft.len() + 1;
+    for _r in 0..(reps/20 + 1) {
+        fft.process_inplace_with_scratch(&mut buffer, &mut scratch);
     }
     let start = Instant::now();
     for _r in 0..reps {
-        fft.process(&mut input, &mut output);
+        fft.process_inplace_with_scratch(&mut buffer, &mut scratch);
     }
     let duration = start.elapsed();
     duration.as_micros() as f32 / reps as f32
 }
-
-
-fn measure_fftw(n:usize) -> f32 {
-    let mut plan: C2CPlan64 = C2CPlan::aligned(&[n], Sign::Forward, Flag::Measure).unwrap();
-    let mut a = AlignedVec::new(n);
-    let mut b = AlignedVec::new(n);
-    let reps = 10000/n + 1;
-    for _r in 0..reps/4 {
-        plan.c2c(&mut a, &mut b).unwrap();
-    }
-    let start = Instant::now();
-    for _r in 0..reps {
-        plan.c2c(&mut a, &mut b).unwrap();
-    }
-    let duration = start.elapsed();
-    duration.as_micros() as f32 / reps as f32
-}
-
-fn measure_chfft(n:usize) -> f32 {
-    let input:  Vec<num_complex::Complex<f64>> = vec![num_complex::Complex::zero(); n];
-    let mut fft = CFft1D::<f64>::with_len(input.len());
-    let reps = 10000/n + 1;
-    for _r in 0..reps/4 {
-        let output = fft.forward(&input[..]);
-    }
-    let start = Instant::now();
-    for _r in 0..reps {
-        let output = fft.forward(&input[..]);
-    }
-    let duration = start.elapsed();
-    duration.as_micros() as f32 / reps as f32
-}
-
-
-fn measure_fourier(n:usize) -> f32 {
-    let mut input:  Vec<Complex<f64>> = vec![Complex::zero(); n];
-    input[0] = Complex::from(1.0);
-    let mut output: Vec<Complex<f64>> = vec![Complex::zero(); n];
-    let fft = create_fft_f64(n);
-    let reps = 10000/n + 1;
-    for _r in 0..reps/4 {
-        fft.transform(&input[..], &mut output[..], Transform::Fft);
-    }
-    let start = Instant::now();
-    for _r in 0..reps {
-        fft.transform(&input[..], &mut output[..], Transform::Fft);
-    }
-    let duration = start.elapsed();
-    duration.as_micros() as f32 / reps as f32
-}
-
 fn main() {
-    println!("N, RustFFT, FFTW, chfft, fourier");
-    for n in 2..256 {
-        let t_rustfft = measure_rustfft(n);
-        let t_fftw = measure_fftw(n);
-        let t_chfft = measure_chfft(n);
-        let t_fourier = measure_fourier(n);
-        println!("{}, {}, {}, {}, {}", n, t_rustfft, t_fftw, t_chfft, t_fourier);
+    let mut planner = FftPlannerAvx::new().unwrap();
+    println!("N, 2xn, 3xn, 4xn, 6xn, 8xn, 9xn, 12xn");
+
+    let mut ns = Vec::new();
+    for power2 in 2..16 {
+        for power3 in 0..10 {
+            for power5 in 0..10 {
+                for power7 in 0..10 {
+                    let n = (3usize.pow(power3) * 5usize.pow(power5) * 7usize.pow(power7)) << power2;
+                    if n > 250 && n < 500000 {
+                        ns.push(n);
+                    }
+                }
+            }
+        }
+    }
+
+    ns.sort();
+
+    for n in ns {
+        let base_fft = planner.plan_fft_forward(n);
+        let time_base = measure_fft(Arc::clone(&base_fft));
+        let time_2xn = measure_fft(Arc::new(MixedRadix2xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        let time_3xn = measure_fft(Arc::new(MixedRadix3xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        let time_4xn = measure_fft(Arc::new(MixedRadix4xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        //let time_5xn = measure_fft(Arc::new(MixedRadix5xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        let time_6xn = measure_fft(Arc::new(MixedRadix6xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        //let time_7xn = measure_fft(Arc::new(MixedRadix7xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        let time_8xn = measure_fft(Arc::new(MixedRadix8xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        let time_9xn = measure_fft(Arc::new(MixedRadix9xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        //let time_11xn = measure_fft(Arc::new(MixedRadix11xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        let time_12xn = measure_fft(Arc::new(MixedRadix12xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        //let time_16xn = measure_fft(Arc::new(MixedRadix16xnAvx::<f32, f32>::new(Arc::clone(&base_fft)).unwrap())) / time_base;
+        println!("{}, {}, {}, {}, {}, {}, {}, {}", n, time_2xn, time_3xn, time_4xn, time_6xn, time_8xn, time_9xn, time_12xn);
     }
 }
 
